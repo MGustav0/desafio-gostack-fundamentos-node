f43672fe93408e6a74b966f49c54119de96fca17
Gustavo Moreira <gustavomoreira88@gmail.com>
2020-06-16 17:43:57 -0500

    Desafio finalizado

---
+++ src/routes/transaction.routes.ts
@@ -0,0 +1,44 @@
+import { Router } from 'express';
+
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import CreateTransactionService from '../services/CreateTransactionService';
+
+const transactionRouter = Router();
+
+const transactionsRepository = new TransactionsRepository();
+
+transactionRouter.get('/', (request, response) => {
+  try {
+    const transactions = transactionsRepository.all();
+    const balance = transactionsRepository.getBalance();
+
+    return response.json({ transactions, balance });
+  } catch (err) {
+    return response.status(400).json({ error: err.message });
+  }
+});
+
+transactionRouter.post('/', (request, response) => {
+  try {
+    // Recebe da requisição
+    const { title, value, type } = request.body;
+
+    // Cria um objeto transação (regra de negócio)
+    const createTransaction = new CreateTransactionService(
+      transactionsRepository,
+    );
+
+    // Executa o objeto -> cria a transação
+    const transaction = createTransaction.execute({
+      title,
+      value,
+      type,
+    });
+
+    return response.json(transaction);
+  } catch (err) {
+    return response.status(400).json({ error: err.message });
+  }
+});
+
+export default transactionRouter;
